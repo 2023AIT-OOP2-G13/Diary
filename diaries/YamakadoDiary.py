b0449c79e43a509fc682df604f2fb3f3afe80e45
te-el <153786738+te-el@users.noreply.github.com>
2023-12-14 15:51:59 +0900

    Update_YamakadoDiary

--- diaries/YamakadoDiary.py
+++ diaries/YamakadoDiary.py
@@ -1,9 +1,9 @@
 from diaries.AbstractDiary import AbstractDiary
-class DiarySample(AbstractDiary):
+class YamakadoDiary(AbstractDiary):
 
     def get_date(self):
         return "2023-12-12"
     def get_summary(self):
         return "今日は家で過ごした。うまくいった。"
     def get_author(self):
-        return "Sample"
+        return "Yamakado"
