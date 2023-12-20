ecbc1e5ef1c02a6f73651d0730aee8d838a068c2
otakumi-bekon <otatakumi2004@gmail.com>
2023-12-20 10:51:35 +0900

    太田Diary追加
    太田Diary追加しました

---
+++ diaries/OtaDiary.py
@@ -0,0 +1,11 @@
+from diaries.AbstractDiary import AbstractDiary
+class NagataniDiary(AbstractDiary):
+    def get_date(self):
+        return "2021-12-09"
+    
+    def get_summary(self):
+
+        return """リーダーめちゃ不安"""
+    
+    def get_author(self):
+        return "Otatakumi"
